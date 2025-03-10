8e51115515ce2444f0437ce7dfdc2bf006ba4423
Esosa <ikponmwose@coventry.ac.uk>
2025-03-10 10:08:37 +0000

    Added more weather data and next day weather data

--- WeatherApp_CW/NVVM/ViewModels/WeatherViewModel.cs
+++ WeatherApp_CW/NVVM/ViewModels/WeatherViewModel.cs
@@ -2,5 +2,14 @@
 
 public class WeatherViewModel
 {
-    
+    private async Task<Location> GetCoordinatesAsync(string address)
+    {
+        IEnumerable<Location> locations = await Geocoding.Default.GetLocationsAsync(address);
+        
+        Location location = locations?.FirstOrDefault();
+
+        if (location != null)
+            Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude:{location.Altitude}");
+        return location;
+    }
 }
