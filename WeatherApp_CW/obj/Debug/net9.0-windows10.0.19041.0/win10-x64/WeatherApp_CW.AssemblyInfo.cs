95644223cf34da45c6d0a24caa0e212c3f8c0729
Esosa <ikponmwose@coventry.ac.uk>
2025-04-10 10:58:20 +0100

    Made improvements in the YourLocationView

--- WeatherApp_CW/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/WeatherApp_CW.AssemblyInfo.cs
+++ WeatherApp_CW/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/WeatherApp_CW.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a8959a6cbeb256982b0b515dc06db2f69e1081f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c7ebb26467d939160c8a97870561e04f24e15b55")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
