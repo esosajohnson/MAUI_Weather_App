5092b8487578fa944e356531bf1df0da84feee5f
Esosa <ikponmwose@coventry.ac.uk>
2025-04-11 16:12:35 +0100

    Resolving Conflicts Commit (IV)

--- WeatherApp_CW/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/WeatherApp_CW.AssemblyInfo.cs
+++ WeatherApp_CW/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/WeatherApp_CW.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cdd47344d2a35cc252559c5cd5a33c42d351d55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+588d4eb8fe868477870a0226c18e8280aad8e9b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
