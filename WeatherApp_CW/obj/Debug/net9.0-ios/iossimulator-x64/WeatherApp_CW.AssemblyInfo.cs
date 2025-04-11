3130349fabfb2e4c4bd4e60d054d7409b10f9cd7
Esosa <ikponmwose@coventry.ac.uk>
2025-04-11 16:06:55 +0100

    Resolving Conflicts (III)

--- WeatherApp_CW/obj/Debug/net9.0-ios/iossimulator-x64/WeatherApp_CW.AssemblyInfo.cs
+++ WeatherApp_CW/obj/Debug/net9.0-ios/iossimulator-x64/WeatherApp_CW.AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cdd47344d2a35cc252559c5cd5a33c42d351d55")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5a9172dce3aa0891e7aba85504bae2149f5e524")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp_CW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp_CW")]
